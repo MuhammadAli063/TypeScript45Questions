fbc415e90f585fe15fc9124bbf4e1d60fcdeb508
Muhammad Ali <momij299@gmail.com>
2024-03-06 02:33:43 +0500

    Magicians

--- Magicians_41_42_43/Magicians.js
+++ Magicians_41_42_43/Magicians.js
@@ -1,9 +1,35 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // Array of magician names
 var magician_names = ['Harry Houdini', 'David Copperfield', 'Penn Jillette', 'Teller'];
 // show_magicians function
-function show_magicians() {
-    for (var i = 0; i < magician_names.length; i++) {
-        console.log(magician_names[i]);
+function show_magicians(names) {
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i]);
     }
 }
-show_magicians();
+// Printing names of magicians
+show_magicians(magician_names);
+// make_great function
+function make_great(names) {
+    var great_magicians = [];
+    for (var j = 0; j < names.length; j++) {
+        great_magicians[j] = "The Great ".concat(names[j]);
+    }
+    return great_magicians;
+}
+// Create a copy of the array and modify the copy
+var great_magicians_array = make_great(__spreadArray([], magician_names, true));
+// Show the original magicians
+console.log("\nOriginal Magicians:");
+show_magicians(magician_names);
+// Show the modified magicians
+console.log("\nGreat Magicians:");
+show_magicians(great_magicians_array);
--- Magicians_41_42_43/Magicians.ts
+++ Magicians_41_42_43/Magicians.ts
@@ -2,12 +2,31 @@
 let magician_names: string[] = ['Harry Houdini', 'David Copperfield', 'Penn Jillette', 'Teller'];
 
 // show_magicians function
-function show_magicians(){
-        for(let i=0; i<magician_names.length; i++){
-            console.log(magician_names[i])
-        }
+function show_magicians(names: string[]) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 }
 
-// printin names of maginasions
-show_magicians()
+// Printing names of magicians
+show_magicians(magician_names);
 
+// make_great function
+function make_great(names: string[]) {
+    let great_magicians: string[] = [];
+    for (let j = 0; j < names.length; j++) {
+        great_magicians[j] = `The Great ${names[j]}`;
+    }
+    return great_magicians;
+}
+
+// Create a copy of the array and modify the copy
+let great_magicians_array = make_great([...magician_names]);
+
+// Show the original magicians
+console.log("\nOriginal Magicians:");
+show_magicians(magician_names);
+
+// Show the modified magicians
+console.log("\nGreat Magicians:");
+show_magicians(great_magicians_array);
